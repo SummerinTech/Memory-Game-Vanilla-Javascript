60059608ff50b0016185c1c0fdbdc775efae7028
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-10-31 22:57:19 -0700

    Inserted image flip
    Started coding javascript functions for game and added in an image flip. Game fits size of console vh.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const cards = [
-	{ name: "blank", img: "/src/images/blank.png" },
 	{ name: "cheeseburger", img: "/src/images/cheeseburger.png" },
 	{ name: "fries", img: "/src/images/fries.png" },
 	{ name: "hotdog", img: "/src/images/hotdog.png" },
 	{ name: "ice-cream", img: "/src/images/ice-cream.png" },
 	{ name: "milkshake", img: "/src/images/milkshake.png" },
 	{ name: "pizza", img: "/src/images/pizza.png" },
-	{ name: "white", img: "/src/images/white.png" },
-	{ name: "blank", img: "/src/images/blank.png" },
 	{ name: "cheeseburger", img: "/src/images/cheeseburger.png" },
 	{ name: "fries", img: "/src/images/fries.png" },
 	{ name: "hotdog", img: "/src/images/hotdog.png" },
 	{ name: "ice-cream", img: "/src/images/ice-cream.png" },
 	{ name: "milkshake", img: "/src/images/milkshake.png" },
 	{ name: "pizza", img: "/src/images/pizza.png" },
-	{ name: "white", img: "/src/images/white.png" },
 ];
 
 cards.sort(() => 0.5 - Math.random());
 console.log(cards);
+
+const grid = document.querySelector(".grid");
+let cardsChosen = [];
+let cardsChosenIds = [];
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let card = document.createElement("img");
+		card.setAttribute("src", "src/images/blank.png");
+		card.setAttribute("data-id", i);
+		card.addEventListener("click", showCard);
+		grid.appendChild(card);
+	}
+
+	console.log(card);
+}
+
+function showCard() {
+	let cardId = this.getAttribute("data-id");
+	cardsChosen.push(cards[cardId].name);
+	cardsChosenIds.push(cardId);
+	this.setAttribute("src", cards[cardId].img);
+	if (cardsChosen.length === 2) {
+		setTimeout(checkForMatch, 500);
+	}
+}
+
+function checkForMatch() {}
+
+createBoard();
